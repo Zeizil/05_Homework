2614a005e3c1aa28ef7c98a2588274f1043a463b
Zeizil <ekphilbrook@gmail.com>
2022-10-31 18:56:26 -0400

    General layout stuff

---
+++ script.js
@@ -0,0 +1,7 @@
+// Put present time at top - start with day, then get the hour
+
+// Look at each event, figure out if they are present, past, or future, and assign them the correct class
+
+//  save button, save to local storage
+
+// get from local storage
