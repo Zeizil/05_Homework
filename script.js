d6ba65b43fa787feec0c31fcbf8973321fe5988d
Zeizil <ekphilbrook@gmail.com>
2022-10-31 19:46:35 -0400

    general set-up of rows, created class for the date, need to set up hour, text box, completion btn

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 // Put present time at top - start with day, then get the hour
+// borrow from the grab-drop exercise for the hour blocks
+var present = moment();
+$("#day").text(present.format("MMMM Do, YYYY"));
 
 // Look at each event, figure out if they are present, past, or future, and assign them the correct class
+// ifs - use current hour, before or after or equal to the present hour
 
-//  save button, save to local storage
+// save button, save to local storage
 
 // get from local storage
